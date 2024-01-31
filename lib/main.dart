25c78589bd5d7894d3e7acb88d9431289c1463a0
Khalid <khalidcalling@gmail.com>
2024-01-31 22:11:26 +0600

    git push

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:meta/meta.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My new App'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text('this is body'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text("Press"),
-          onPressed: () => print("Pressed"),
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('This is title'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text('This is the body'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Text('Click'),
+            onPressed: () => print("Pressed"),
+          ),
         ),
       ),
-    ));
+    );
