524ce8fef1a3dc8036709d554e0825293d86468e
Khalid <khalidcalling@gmail.com>
2024-02-01 20:12:35 +0600

    git push

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,35 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:meta/meta.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('This is title'),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Text('This is the body'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Text('Click'),
-            onPressed: () => print("Pressed"),
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'This is title',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w900,
+            color: Color.fromARGB(255, 231, 248, 246),
+            fontFamily: 'Lora',
           ),
         ),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 3, 1, 95),
+      ),
+      body: Center(
+        child: Text('This is the body'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color.fromARGB(255, 151, 212, 37),
+        onPressed: () => print("Pressed"),
+        child: Text('Click'),
       ),
     );
+  }
+}
