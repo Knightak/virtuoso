ff954b878ba12488a88c34e370120407fe83e8ba
Khalid <khalidcalling@gmail.com>
2024-02-02 17:35:45 +0600

    git push

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,76 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+const String imageUrl =
+    'https://cdn.pixabay.com/photo/2016/03/18/15/02/ufo-1265186_1280.jpg';
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 24, 2, 100),
         title: Text(
-          'This is Title',
+          'VIRTUOSO',
+          textAlign: TextAlign.center,
           style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w900,
-            color: Color.fromARGB(255, 231, 248, 246),
-            fontFamily: 'Lora',
+            fontSize: 15,
+            color: Colors.amber,
           ),
         ),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text('Click'),
+        child: Column(
+          //mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.network(
+              imageUrl,
+              width: 300,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlutterLogo(
+                  size: 50,
+                ),
+                Text(
+                  'This is tutuor',
+                  style: TextStyle(
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              'This is app',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.red,
+                fontFamily: 'Lora',
+              ),
+            ),
+            Text(
+              'This is ios',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.red,
+                fontFamily: 'Lora',
+              ),
+            ),
+            Text(
+              'This is new line',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.red,
+                fontFamily: 'Lora',
+              ),
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color.fromARGB(255, 151, 212, 37),
-        onPressed: () => print("Pressed"),
-        child: Text('Click'),
-      ),
-    );
-  }
+    ),
+  ));
 }
