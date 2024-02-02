36f47e73f657ea719401dab9d41b7f2b517f7a64
Khalid <khalidcalling@gmail.com>
2024-02-02 21:36:23 +0600

    git push

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 const String imageUrl =
-    'https://cdn.pixabay.com/photo/2016/03/18/15/02/ufo-1265186_1280.jpg';
+    'https://cdn.pixabay.com/photo/2024/01/30/02/06/winter-8541095_960_720.jpg';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
+        leading: Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 24, 2, 100),
         title: Text(
@@ -19,58 +23,114 @@ void main() {
         ),
       ),
       body: Center(
-        child: Column(
-          //mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.network(
-              imageUrl,
-              width: 300,
-              height: 300,
-              fit: BoxFit.cover,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                FlutterLogo(
-                  size: 50,
+                Image.network(
+                  imageUrl,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
                 ),
-                Text(
-                  'This is tutuor',
-                  style: TextStyle(
-                    color: Colors.green,
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Coxs Bazar',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            Text(
+                              'City in Bangladesh',
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  '41',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 13, 187, 218),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-            Text(
-              'This is app',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.red,
-                fontFamily: 'Lora',
-              ),
-            ),
-            Text(
-              'This is ios',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.red,
-                fontFamily: 'Lora',
-              ),
-            ),
-            Text(
-              'This is new line',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.red,
-                fontFamily: 'Lora',
-              ),
-            ),
-          ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.call,
+                          color: Colors.deepOrange,
+                        ),
+                        Text('Call'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.place,
+                          color: Colors.deepOrange,
+                        ),
+                        Text('Route'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.share,
+                          color: Colors.deepOrange,
+                        ),
+                        Text('Share'),
+                      ],
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(28.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        longtext,
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
         ),
       ),
     ),
   ));
 }
+
+const String longtext =
+    '''Cox’s Bazar is a town on the southeast coast of Bangladesh. It’s known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.Cox’s Bazar is a town on the southeast coast of Bangladesh. It’s known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.''';
